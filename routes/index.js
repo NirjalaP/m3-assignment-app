92ada8f29e5aab16e231d6f7daf05d8e46532d6b
NirjalaP <nirjala.prajapati11@gmail.com>
2025-03-18 07:24:50 -0700

    Fetched and displayed registrations in the listings view

--- routes/index.js
+++ routes/index.js
@@ -38,8 +38,14 @@ const mongoose = require("mongoose")
    }
  );
 
- router.get('/registrations', (req, res) => {
-    res.render('index', { title: 'Listing registrations' });
+ router.get("/registrations", (req, res) => {
+    Registration.find()
+      .then((registrations) => {
+        res.render("index", { title: "Listing registrations", registrations });
+      })
+      .catch(() => {
+        res.send("Sorry! Something went wrong.");
+      });
 });
 
  module.exports = router;
